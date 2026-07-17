30bcc391f09779abd1a3888b20300a5bf0091ae9
Murk channa <areeshamurk@gmail.com>
2026-07-17 21:33:51 +0500

    fix: correct middleware path in app.js

--- task2/src/app.js
+++ task2/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const logger = require("../../middleware/logger");
+const logger = require("../middleware/logger");
+
 const tasksRouter = require("./tasks");
 
 const app = express();
